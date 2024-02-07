98ff73f43a6219a3b3ad1a17b51de759b6932c0d
Alessandro <a.formigoni@studenti.unibs.it>
2024-02-07 10:33:43 +0100

    updated stuff

--- freq_track.m
+++ freq_track.m
@@ -1,32 +1,32 @@
-function [x_up, P_up] = freq_track(dt,yMeas,x,P,lambda)
+function [x_up,xhat_t, P_up, Kt, s, e_t] = freq_track(dt,yMeas,x,P,r,q,w)
 H = [1 0 0];
-I = diag([0 0 dt]);
+I = diag([w*dt w*dt q*dt]);
 %~
     %errore
     e_t = yMeas - H*x;
 
     %Guadagno del filtro
-    Kt = P*H' / (H*P*H' + lambda);
-
+    s = (H*P*H' + r);
+    Kt = P*H' / s;
+    
     %Correzione
     xhat_t = x + Kt.*e_t;
 
     %Measurement update
-    F = linearize(dt.*xhat_t);
-    P_up = F*(P-Kt*H*P)*F'+ I;
+    F = linearize(xhat_t,dt);
+    L = chol(F*(P-Kt*H*P)*F'+ I);
+    P_up = L'*L;
 
-    x_up = f([xhat_t(1) xhat_t(2) xhat_t(3)*dt]);
-    x_up(3) = xhat_t(3);
-
+    x_up = f(xhat_t,dt);
 end
 
-function sigmodel = f(x)
-sigmodel = [cos(x(3))*x(1)-sin(x(3))*x(2); sin(x(3))*x(1)+cos(x(3))*x(2); x(3)];
+function sigmodel = f(x,dt)
+sigmodel = [cos(dt*x(3))*x(1)-sin(dt*x(3))*x(2); sin(dt*x(3))*x(1)+cos(dt*x(3))*x(2); x(3)];
 end
 
-function Ft = linearize(xhat)
+function Ft = linearize(xhat,dt)
 Ft = zeros(3,3);
-Ft(1,:) = [cos(xhat(3)) -sin(xhat(3)) -sin(xhat(3))*xhat(1)-cos(xhat(3))*xhat(2)];
-Ft(2,:) = [sin(xhat(3)) cos(xhat(3)) cos(xhat(3))*xhat(1)- sin(xhat(3))*xhat(2)];
+Ft(1,:) = [cos(dt*xhat(3)) -sin(dt*xhat(3)) dt*(-sin(dt*xhat(3))*xhat(1)-cos(dt*xhat(3))*xhat(2))];
+Ft(2,:) = [sin(dt*xhat(3)) cos(dt*xhat(3)) dt*(cos(dt*xhat(3))*xhat(1)-sin(dt*xhat(3))*xhat(2))];
 Ft(3,:) = [0 0 1];
 end
--- ft_audio.m
+++ ft_audio.m
@@ -1,37 +1,44 @@
 clear
 clc
 
-filename = "fick_yer_mini_dbass.wav";
+filename = "./music/1000.mp4";
 [y, Fs] = audioread(filename);
-dt = 2*pi/Fs;
+dt = 1/(Fs);
 nsampl = length(y(:,1));
 x = zeros(3,nsampl);
 P = zeros(3,3,nsampl);
 yMeasl = zeros(1,nsampl);
 yMeasr = zeros(1,nsampl);
-omega = 110;
-
+s = zeros(1,nsampl-1);
+e = zeros(1,nsampl-1);
+omega = 67*(2*pi);
+t = 0:dt:(dt*nsampl);
 xl(:,1) = [0 0 omega];
 Pl(:,:,1) = eye(3);
-xr(:,1) = [0 0 omega];
-Pr(:,:,1) = eye(3);
-
-r = 1e-2; lambda = 0.003;
-
+% xr(:,1) = [0 0 omega];
+% Pr(:,:,1) = eye(3);
+r = 0.011; lambda = 800; w = 7e-4;
+%vl = 4*pinknoise(nsampl);
+vr = wgn(1,nsampl,10*log10(r));
 for k = 1:nsampl-1
-yMeasl(k) = y(k,1) + r*sin(100/201*k);%wgn(1,1,10*log10(r));
-yMeasr(k) = y(k,2) + wgn(1,1,10*log10(r));
-[xl(:,k+1), Pl(:,:,k+1)] = freq_track(dt,yMeasl(k),xl(:,k),Pl(:,:,k),lambda);
-[xr(:,k+1), Pr(:,:,k+1)] = freq_track(dt,yMeasr(k),xr(:,k),Pr(:,:,k),lambda);
+yMeasl(k) = y(k,1) + 0*vr(k);
+% yMeasr(k) = y(k,2) + vr(k);
+[xl(:,k+1),xl(:,k), Pl(:,:,k+1),~,s(k),e(k)] = freq_track(dt,yMeasl(k),xl(:,k),Pl(:,:,k),r,lambda,w);
+% [xr(:,k+1),xr(:,k), Pr(:,:,k+1)] = freq_track(dt,yMeasr(k),xr(:,k),Pr(:,:,k),r,lambda);
 end
 
 figure(1)
-plot((xl(3,:)))
+plot(t(1:length(t)-1),(xl(3,:)./(2*pi)))
+xlabel('t')
+ylabel('Hz')
+legend('frequenza stimata','frequenza reale')
 grid on
 figure(2)
 hold on
 plot(y);
-xs = [xl(1,:); xr(1,:)];
+xs = [xl(1,:)];
+figure(3)
+pspectrum(y(:,1), Fs, 'spectrogram', 'FrequencyLimits', [20 1500], 'OverlapPercent',0,'Leakage',0.20,'MinThreshold',-45);
 %plot(xs);
 %grid on
 %sound(yMeas,Fs);
